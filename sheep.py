52e43c4aa448dac35550e00d075eaded227bf629
烟花小神 <1347229508@qq.com>
2022-09-19 00:34:58 +0800

    修改登录失败
    修改登录失败

--- sheep.py
+++ sheep.py
@@ -34,7 +34,6 @@ nick_name = "默认用户"
 res = session.get(
     url=f"https://cat-match.easygame2021.com/sheep/v1/game/user_info?uid={uid}&t=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ0NzgwMTAsIm5iZiI6MTY2MzM3NTgxMCwiaWF0IjoxNjYzMzc0MDEwLCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjo0ODUwNDY1MCwiZGVidWciOiIiLCJsYW5nIjoiIn0.IEjNoHJiJPqlh86DqDS3-SMTwErTCatQF6ykZk4o-Yc", verify=False).json()
 openId = res['data']['wx_open_id']
-nick_name = res['data']['nick_name']
 avatar = res['data']['avatar']
 # ***********************UID**END****************************
 
